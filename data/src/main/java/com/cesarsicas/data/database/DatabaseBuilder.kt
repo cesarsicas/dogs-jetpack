67bf55ac3b68d273c48d0e1d6d0890979e86b16e
Júlio Siqueira <jctsiqueira@gmail.com>
2019-10-04 17:45:29 -0300

    including code to populate db

--- data/src/main/java/com/cesarsicas/data/database/DatabaseBuilder.kt
+++ data/src/main/java/com/cesarsicas/data/database/DatabaseBuilder.kt
@@ -1,18 +1,37 @@
 package com.cesarsicas.data.database
 
 import android.app.Application
+import androidx.annotation.NonNull
 import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import java.util.concurrent.Executors
 
 object DatabaseBuilder {
     private const val databaseName = "dogs_jetpack_db"
 
-    fun build(applicationContext:Application): AppDatabase {
+    fun build(applicationContext: Application): AppDatabase {
 
         //todo shouldnt allow queries on main thread, just for testing
 
         return Room.databaseBuilder(
             applicationContext,
-            AppDatabase::class.java, databaseName).allowMainThreadQueries().build()
+            AppDatabase::class.java,
+            databaseName
+        )
+            .addCallback(object : RoomDatabase.Callback() {
+                override fun onCreate(@NonNull db: SupportSQLiteDatabase) {
+                    super.onCreate(db)
+                    Executors.newSingleThreadScheduledExecutor().execute(object : Runnable {
+                        override fun run() {
+                            // insert data using DAO
+                            //todo include code to populate database
+                        }
+                    })
+                }
+            })
+            .allowMainThreadQueries().build()
+
     }
 
 }
